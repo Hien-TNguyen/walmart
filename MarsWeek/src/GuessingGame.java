a11fa7ec1025d6d1aab9ca828600133a10c94d7d
Hien Nguyen <hiennguyen83@gmail.com>
2023-07-02 07:29:19 -0700

    HW1: part 2 and 3

---
+++ MarsWeek/src/GuessingGame.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessingGame {
+
+  public void GuessingGame() {}
+
+  public void PlayGame() {
+    PrintWelcomeMessage();
+    String name = GetPlayerName();
+    int target = PickRandomNumber();
+    int guessCount = RunGuessingLoop(target, name);
+    PrintResult(guessCount, name);
+  }
+
+  private void PrintWelcomeMessage() {
+    System.out.println("Welcome to the Guessing Game!");
+
+  }
+
+  private String GetPlayerName() {
+    System.out.println("Please type your name.");
+    Scanner scan = new Scanner(System.in);
+    String name = scan.nextLine();
+    return name;
+  }
+
+  private int PickRandomNumber() {
+    Random rand = new Random();
+    int number = rand.nextInt(100);
+    return number; // Guaranteed random, picked by fair dice roll.
+  }
+
+
+  private int RunGuessingLoop(int target, String name) {
+    int guessCount = 0;
+    System.out.println("Try to guess my number.");
+    while (true) {
+      String input = inputScanner.next();
+      try {
+        int intGuess = Integer.parseInt(input);
+        System.out.println(String.format("Your guess? %d", intGuess));
+        guessCount += 1;
+        if (intGuess < target) {
+          System.out.println("Your guess is low.");
+        } else if (intGuess > target) {
+          System.out.println("Your guess is high.");
+        } else {
+          break;
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("It is not an integer. Try again!");
+        continue;
+      }
+    }
+    return guessCount;
+  }
+
+
+  private void PrintResult(int guessCount, String name) {
+    System.out.println(String.format("Well done, %s! You found my number in %d tries!", name, guessCount));
+  }
+
+
+  private Scanner inputScanner = new Scanner(System.in);
+  
+}
---
+++ MarsWeek/src/Mars.java
@@ -0,0 +1,47 @@
+public class Mars {
+  public static void main(String[] args) throws InterruptedException{
+    String colonyMars = "Jupiter";
+    int shipPopulation = 300;
+    double meals = 4000.00;
+    boolean landing = true;
+
+    // meals remaining after two days
+    meals = meals - 2 * (shipPopulation * 0.75);
+    System.out.println(meals);
+
+    // meals increases 50%
+    meals += meals / 2;
+
+    // shipPopulation is increased by 5 more babies
+    shipPopulation += 5;
+
+    String landingLocation = "The Ocean";
+
+    if (landingLocation == "The Plain") {
+      System.out.println("Bbzzz Landing on the Plain");
+    } else {
+      System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
+    }
+
+    landing = landingCheck(10);
+    GuessingGame game = new GuessingGame();
+    game.PlayGame();
+  }
+
+  public static boolean landingCheck(int minutesLeft) throws InterruptedException {
+    for (int minute = 0; minute <= minutesLeft; minute ++) {
+      if ((minute % 2 == 0) && (minute % 3 == 0)) {
+        System.out.println("Keep Center");
+      }else if (minute % 2 == 0) {
+        System.out.println("Right");
+      } else if (minute % 3 == 0) {
+        System.out.println("Left");
+      } else {
+        System.out.println("Calculating");
+      }
+      Thread.sleep(250);
+    }
+    System.out.println("Landed");
+    return false;
+  }
+}
