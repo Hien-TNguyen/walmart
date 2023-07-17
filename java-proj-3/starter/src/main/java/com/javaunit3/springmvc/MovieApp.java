6da6c3d699dac59f586a3f52dffc09cebb84a7ae
Hien Nguyen <hiennguyen83@gmail.com>
2023-07-17 11:10:19 -0700

    HW3 springboot part 1

---
+++ java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieApp.java
@@ -0,0 +1,23 @@
+package com.javaunit3.springmvc;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan
+public class MovieApp {
+  public static void main(String[] args) {
+    // create application context 
+    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieApp.class);
+    
+    // get the best moview using default bean id
+    BestMovieService bestMovieService = applicationContext.getBean("bestMovieService", BestMovieService.class);
+
+    // using the best movie service, get the best movie
+    Movie bestMovie = bestMovieService.getBestMovie();
+
+    // print out the title, maturity rating, genre of the best movie
+    System.out.println("Title: " + bestMovie.getTitle());
+    System.out.println("Maturity Rating: " + bestMovie.getMaturityRating());
+    System.out.println("Genre: " + bestMovie.getGenre());
+  }
+}
